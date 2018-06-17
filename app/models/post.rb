eb7daf1dd91996392f966092938c6bdaf527b27d
Kevin Huang <yellow.khaos@gmail.com>
2018-06-17 02:01:21 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,3 @@ class Post < ActiveRecord::Base
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
 end
-
