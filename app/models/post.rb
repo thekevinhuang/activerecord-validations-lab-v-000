15398e4f93396171ff88d8fc7bab9852ed378324
Kevin Huang <yellow.khaos@gmail.com>
2018-06-17 02:01:28 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+class ClickbaitValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.title =~ /Won't Believe|Secret|Guess|Top [0-9]/i
+      record.errors[:title] << 'Needs more Clickbait'
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with ClickbaitValidator
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
 end
