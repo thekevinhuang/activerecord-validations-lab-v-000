6f2d8379bb67ff4f8760779231f0231d0d5650cc
Kevin Huang <yellow.khaos@gmail.com>
2018-06-17 01:37:46 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,18 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with ClickbaitValidator
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+
+  
+end
+
+class ClickbaitValidator < ActiveModel::Validator
+  def validate(record)
+    if record.
+      
+    end
+  end
 end
